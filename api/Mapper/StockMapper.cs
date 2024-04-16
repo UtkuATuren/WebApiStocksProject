f577f8b9d5245773346bb56f6326c281e0a66439
UATHUB <utkuaturen@gmail.com>
2024-04-16 13:40:17 +0300

    Proje dosyası ekle.

---
+++ api/Mapper/StockMapper.cs
@@ -0,0 +1,35 @@
+﻿using api.Dtos.Stock;
+using api.models;
+
+namespace api.Mapper
+{
+    public static class StockMapper
+    {
+        public static StockDto ToStockDto(this Stock stock)
+        {
+            return new StockDto
+            {
+                Id = stock.Id,
+                Symbol = stock.Symbol,
+                CompanyName = stock.CompanyName,
+                Purchase = stock.Purchase,
+                LastDiv = stock.LastDiv,
+                Industry = stock.Industry,
+                MarketCap = stock.MarketCap,
+            };
+        }
+
+        public static Stock ToStockFromCreateDto(this CreateStockRequestDto createDto)
+        {
+            return new Stock
+            {
+                Symbol = createDto.Symbol,
+                CompanyName = createDto.CompanyName,
+                Purchase = createDto.Purchase,
+                LastDiv = createDto.LastDiv,
+                Industry = createDto.Industry,
+                MarketCap = createDto.MarketCap,
+            };
+        }
+    }
+}
